f5c6c3e5463ad18c70e569c7740346d73162d7ab
DeuxEx <per@wennergrund.net>
2026-08-09 09:42:10 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 mod patterns;
 use patterns::findpatterns;
+mod watchdog;
+use watchdog::read_last_line;
 
 
 
@@ -48,23 +50,6 @@ fn ensure_fifo_exists(pipe_path: &str) -> std::io::Result<()> {
 
 
 
-/// Läser den sista icke-tomma raden ur en fil genom att söka sig bakåt från slutet
-fn read_last_line<P: AsRef<Path>>(path: P) -> std::io::Result<Option<String>> {
-    let mut file = File::open(path)?;
-    let file_size = file.metadata()?.len();
-
-    if file_size == 0 {
-        return Ok(None);
-    }
-
-    let read_size = file_size.min(1024) as i64;
-    file.seek(SeekFrom::End(-read_size))?;
-
-    let reader = BufReader::new(file);
-    let lines: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
-
-    Ok(lines.into_iter().rev().find(|line| !line.trim().is_empty()))
-}
 
 
 
---
+++ src/watchdog.rs
@@ -0,0 +1,27 @@
+
+
+use std::{array, fs::{File, OpenOptions}};
+use std::io::{BufRead, BufReader, Seek, SeekFrom, Write};
+use std::path::Path;
+
+
+
+
+
+/// Läser den sista icke-tomma raden ur en fil genom att söka sig bakåt från slutet
+pub fn read_last_line<P: AsRef<Path>>(path: P) -> std::io::Result<Option<String>> {
+    let mut file = File::open(path)?;
+    let file_size = file.metadata()?.len();
+
+    if file_size == 0 {
+        return Ok(None);
+    }
+
+    let read_size = file_size.min(1024) as i64;
+    file.seek(SeekFrom::End(-read_size))?;
+
+    let reader = BufReader::new(file);
+    let lines: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
+
+    Ok(lines.into_iter().rev().find(|line| !line.trim().is_empty()))
+}
