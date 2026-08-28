11dbf53e12936d3db73cb1196b3a5f88ae752acb
root <par@wennergrund.net>
2026-08-28 19:32:53 +0200

    struggeling with fileopen from struct vars

--- src/main.rs
+++ src/main.rs
@@ -104,8 +104,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //let mut tailer = LineTailer::new(TARGET_FILE)?;
     let mut tailer = LineTailer::new(config.target_file.clone())?;
 
-    println!("{}", config.target_file.clone());
-
+    //println!("{}", config.target_file.clone());
+    assert!(!fs::exists(config.target_file.clone()).expect("Cant check existence of file"));
 
 
     // Loopa igenom radläsning från slutet med 1ms fördröjning
